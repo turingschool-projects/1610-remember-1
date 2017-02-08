58548fdfccaa5df4030c79203dc06015a33bb41f
bekahlundy <lundyrebekah@gmail.com>
2017-02-08 16:58:15 -0700

    Switch over to using mirage

--- app/routes/reminders.js
+++ app/routes/reminders.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
   model() {
-    return ['bekah needs to get more banana chips', 'josh needs to do a cartwheel', 'Bekah needs to get out of her funk', 'We both need to listen to some funky music', 'make money']
+    return this.get('store').findAll('reminder');
   }
 });
