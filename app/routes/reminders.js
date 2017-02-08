e0d411b37f4c57805186caed97434c0a4fefd53d
bekahlundy <lundyrebekah@gmail.com>
2017-02-07 17:07:47 -0700

    Tests running on server, one is failing and one is passing

--- app/routes/reminders.js
+++ app/routes/reminders.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return ['bekah needs to get more banana chips', 'josh needs to do a cartwheel']
+    return ['bekah needs to get more banana chips', 'josh needs to do a cartwheel', 'Bekah needs to get out of her funk', 'We both need to listen to some funky music', 'make money']
   }
 });
