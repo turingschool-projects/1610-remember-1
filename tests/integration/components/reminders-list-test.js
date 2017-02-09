956f21f344848aa427f3a8c397cbefcc1c586af3
bekahlundy <lundyrebekah@gmail.com>
2017-02-09 13:12:53 -0700

    Integration test checking for ul passing

--- tests/integration/components/reminders-list-test.js
+++ tests/integration/components/reminders-list-test.js
@@ -2,28 +2,30 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('reminders-list', 'Integration | Component | reminders list', {
-  integration: true
+  integration: true,
+
+  beforeEach: function () {
+    this.set('reminder', { title: 'josh', date: 'date', notes: 'josh needs 20 dogs'})
+  }
 });
 
-test('it renders', function(assert) {
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
+test('it renders an ul', function(assert) {
   this.render(hbs`{{reminders-list}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#reminders-list}}
-      template block text
-    {{/reminders-list}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('ul').length, 1);
+  // // Template block usage:
+  // this.render(hbs`
+  //   {{#reminders-list}}
+  //     template block text
+  //   {{/reminders-list}}
+  // `);
+  // assert.equal(this.$().text().trim(), 'template block text');
 });
 
-test('it renders an h1 tag', () => {
-  
-})
+// test('default route renders a title', () => {
+//   this.render(hbs`{{reminders-list}}`);
+//   assert
+//   andThen(() => {
+//     equal(find('h1')).length, 1, 'The first page renders an h1'
+//   })
+// })
