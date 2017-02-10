ee597dca80549ddafcee886e32d2ab69b927dfdc
Joshua Small <jksmall0631@gmail.com>
2017-02-10 16:42:57 -0700

    Add dynamic route for reminder cards

--- app/components/reminders-list.js
+++ app/components/reminders-list.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  actions: {
-    test() {
-      alert('bla!')
-    }
-  }
+  // actions: {
+  //   display() {
+  //     alert('bla!')
+  //   }
+  // }
 });
