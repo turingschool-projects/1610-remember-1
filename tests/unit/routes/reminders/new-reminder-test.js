79331da2502126da077f63e51bc93c52a235bd5a
Joshua Small <jksmall0631@gmail.com>
2017-02-11 18:55:54 -0700

    Create new reminder functionality

--- tests/unit/routes/reminders/new-reminder-test.js
+++ tests/unit/routes/reminders/new-reminder-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:reminders/new', 'Unit | Route | reminders/new', {
+moduleFor('route:reminders/new-reminder', 'Unit | Route | reminders/new reminder', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
