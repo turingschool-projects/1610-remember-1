5b3b234ed810a9b0d67e7c38fa3781afcd0aa630
bekahlundy <lundyrebekah@gmail.com>
2017-02-14 18:35:45 -0700

    Add delete functionality to reminder card

--- app/components/reminder-card.js
+++ app/components/reminder-card.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  actions: {
+    removeReminder(model) {
+      model.destroyRecord();
+    }
+  }
 });
--- app/components/reminders-list.js
+++ app/components/reminders-list.js
@@ -3,10 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
 
-  title: '',
-  date: '',
-  notes: '',
-
   actions: {
     removeReminder(reminder) {
       reminder.destroyRecord();
