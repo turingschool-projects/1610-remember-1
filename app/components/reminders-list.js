1182b5f58368bb4f9654c07f6704dd1200f0c134
bekahlundy <lundyrebekah@gmail.com>
2017-02-14 18:28:04 -0700

    Create actions and function to destroy reminders

--- app/components/reminders-list.js
+++ app/components/reminders-list.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+  store: Ember.inject.service(),
+
+  title: '',
+  date: '',
+  notes: '',
+
+  actions: {
+    removeReminder(reminder) {
+      reminder.destroyRecord();
+    }
+  }
 });
